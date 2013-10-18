8fed19c6455634e7a107ded3761ad8a4f0f5b2f6
zhuangyinping <zhuangyinping@gmail.com>
2013-10-18 23:48:54 +0800

    one to one  one direction

--- grails-app/domain/com/mapping/Car.groovy
+++ grails-app/domain/com/mapping/Car.groovy
@@ -7,12 +7,12 @@ class Car {
 }
 
 /**
-| car   | CREATE TABLE `car` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `version` bigint(20) NOT NULL,
-  `engine_id` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK17FD4FBE0BC7` (`engine_id`),
-  CONSTRAINT `FK17FD4FBE0BC7` FOREIGN KEY (`engine_id`) REFERENCES `engine` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 |
+ | car   | CREATE TABLE `car` (
+ `id` bigint(20) NOT NULL AUTO_INCREMENT,
+ `version` bigint(20) NOT NULL,
+ `engine_id` bigint(20) NOT NULL,
+ PRIMARY KEY (`id`),
+ KEY `FK17FD4FBE0BC7` (`engine_id`),
+ CONSTRAINT `FK17FD4FBE0BC7` FOREIGN KEY (`engine_id`) REFERENCES `engine` (`id`)
+ ) ENGINE=InnoDB DEFAULT CHARSET=latin1 |
 */
--- grails-app/domain/com/mapping/Engine.groovy
+++ grails-app/domain/com/mapping/Engine.groovy
@@ -1,7 +1,7 @@
 package com.mapping
 
 class Engine {
-    Car car
+    static belongsTo = [car: Car]
     static constraints = {
     }
 }
@@ -10,9 +10,6 @@ class Engine {
  | engine | CREATE TABLE `engine` (
  `id` bigint(20) NOT NULL AUTO_INCREMENT,
  `version` bigint(20) NOT NULL,
- `car_id` bigint(20) DEFAULT NULL,
- PRIMARY KEY (`id`),
- KEY `FKB297FA426C2E30D` (`car_id`),
- CONSTRAINT `FKB297FA426C2E30D` FOREIGN KEY (`car_id`) REFERENCES `car` (`id`)
- ) ENGINE=InnoDB DEFAULT CHARSET=latin1 |
+ PRIMARY KEY (`id`)
+ ) ENGINE=InnoDB DEFAULT CHARSET=latin1
  */
